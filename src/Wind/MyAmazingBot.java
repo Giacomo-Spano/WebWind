7e72888d52ea12cddf086227ae8651fe1a8af07d
GIACOMO SPANO <giacomo.spano@libero.it>
2017-07-22 16:05:05 +0200

    message

--- src/Wind/MyAmazingBot.java
+++ src/Wind/MyAmazingBot.java
@@ -176,8 +176,15 @@ public class MyAmazingBot extends TelegramLongPollingBot {
         String speed = "";
         String avspeed = "";
         String direction = "";
-        String time = "";
+        String times = "";
         int count = 0;
+
+        double maxspeed = 0;
+        for (MeteoStationData md : list) {
+            if (maxspeed < md.speed) maxspeed = md.speed;
+            if (maxspeed < md.averagespeed) maxspeed = md.averagespeed;
+        }
+
         for (MeteoStationData md : list) {
 
             double x = (md.datetime.getTime() - min) * 100 / (max - min);
@@ -192,20 +199,44 @@ public class MyAmazingBot extends TelegramLongPollingBot {
             avspeed += md.averagespeed;
 
             if (count > 0) direction += ",";
-            direction += md.directionangle;
+            direction += md.directionangle * maxspeed / 360.0;
 
-            if (count++ % 5 == 0) {
-                time += "%7C" + df.format(md.datetime);
-            }
-
+            count++;
         }
 
+        Calendar calcounter = Calendar.getInstance();
+        calcounter.setTime(startTime);
+        Date cursor = startTime;
+        while (endTime.after(cursor) || endTime.equals(cursor)) {
+            times += "|" + df.format(cursor);
+            calcounter.add(Calendar.MINUTE, 30);
+            cursor = calcounter.getTime();
+
+        }
+        times += "|";
+
         String str = "";
-        str = xserie + "|" + speed + "|" + avspeed;
+        String chd = "&chd=t:" + xserie + "|" + speed + "|" + xserie + "|" + avspeed + "|" + xserie + "|" + direction;
+        String chs = "&chs=700x250";
+        String chg = "&chg=10,10";
+        //String chxl = "&chxl=0:|Freezing|C|kk|kk|Hot|2:|S|E|N|O|";
+        String chxl = "&chxl=0:" + times + "2:|E|N|O|S|";
+
+        //String sourceimage = "https://chart.googleapis.com/chart?chxt=x,y,r&chds=a&cht=lxy&chco=FF0000,00FF00,0000FF&chd=t:0.0,2.0,4.0,7.0,9.0,11.0,16.0,18.0,20.0,22.0,25.0,27.0,30.0,31.0,34.0,36.0,39.0,40.0,43.0,45.0,48.0,50.0,52.0,54.0,57.0,59.0,61.0,63.0,66.0,68.0,69.0,72.0,75.0,77.0,80.0,81.0,84.0,86.0,89.0,90.0,93.0,95.0,98.0,100.0|0.0,0.0,0.0,0.0,0.0,1.7,0.0,0.0,0.0,1.7,0.0,3.1,1.7,0.0,0.0,0.0,0.0,1.7,0.0,1.7,0.0,1.7,1.7,3.1,1.7,1.7,0.0,0.0,0.0,0.0,1.7,0.0,1.7,0.0,0.0,0.0,0.0,1.7,0.0,0.0,0.0,0.0,0.0,0.0|0.0,2.0,4.0,7.0,9.0,11.0,16.0,18.0,20.0,22.0,25.0,27.0,30.0,31.0,34.0,36.0,39.0,40.0,43.0,45.0,48.0,50.0,52.0,54.0,57.0,59.0,61.0,63.0,66.0,68.0,69.0,72.0,75.0,77.0,80.0,81.0,84.0,86.0,89.0,90.0,93.0,95.0,98.0,100.0|1.7,1.7,1.7,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.7,1.7,1.7,1.7,1.7,1.7,0.0,0.0,0.0,1.7,1.7,1.7,1.7,1.7,0.0,0.0,0.0,0.0,0.0,0.0,0.0&chs=700x250&chg=10,10&chxl=0:|Freezing|C|kk|kk|Hot|2:|S|E|N|O|";
+
+        //String chd = "&chd=t:0.0,2.0,4.0,7.0,9.0,11.0,16.0,18.0,20.0,22.0,25.0,27.0,30.0,31.0,34.0,36.0,39.0,40.0,43.0,45.0,48.0,50.0,52.0,54.0,57.0,59.0,61.0,63.0,66.0,68.0,69.0,72.0,75.0,77.0,80.0,81.0,84.0,86.0,89.0,90.0,93.0,95.0,98.0,100.0|0.0,0.0,0.0,0.0,0.0,1.7,0.0,0.0,0.0,1.7,0.0,3.1,1.7,0.0,0.0,0.0,0.0,1.7,0.0,1.7,0.0,1.7,1.7,3.1,1.7,1.7,0.0,0.0,0.0,0.0,1.7,0.0,1.7,0.0,0.0,0.0,0.0,1.7,0.0,0.0,0.0,0.0,0.0,0.0|0.0,2.0,4.0,7.0,9.0,11.0,16.0,18.0,20.0,22.0,25.0,27.0,30.0,31.0,34.0,36.0,39.0,40.0,43.0,45.0,48.0,50.0,52.0,54.0,57.0,59.0,61.0,63.0,66.0,68.0,69.0,72.0,75.0,77.0,80.0,81.0,84.0,86.0,89.0,90.0,93.0,95.0,98.0,100.0|1.7,1.7,1.7,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.7,1.7,1.7,1.7,1.7,1.7,0.0,0.0,0.0,1.7,1.7,1.7,1.7,1.7,0.0,0.0,0.0,0.0,0.0,0.0,0.0";
+
+        String sourceimage = "https://chart.googleapis.com/chart?chxt=x,y,r&chds=a&cht=lxy&chco=FF0000,00FF00,0000FF" +
+                chd +
+                chs +
+                chg +
+                chxl;
+                //"&chs=700x250&chg=10,10&chxl=0:|Freezing|C|kk|kk|Hot|2:|S|E|N|O|";
+
 
         //String filepath = System.getenv("tmp") + "/" + "chartx" + ".jpg";
         String filepath = Core.getTmpDir() + "/" + "chartx" + ".jpg";
-        Core.getImage("https://image-charts.com/chart?cht=lxy&chs=700x125&chd=t:1,2,3,4|10,50,4,44|1,2,3,4|3,33,3,10&chxt=x,y&chxl=0:%7C1%7C2v%7C3%7C4:1:%7C1%7C2v%7C3%7C4:",filepath);
+        Core.getImage(sourceimage,filepath);
 
 
         return filepath;
